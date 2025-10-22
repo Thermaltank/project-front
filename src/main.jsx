0c78569f57b986aa8770cfb8da3d963f13f67180
Ronald <Ronaldiglesiasgonzalez@gmail.com>
2025-10-22 06:24:04 -0500

    fixed Main

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import MainRoute from './routes/MainRoute.jsx'
 import { AuthProvider } from './auth/AuthContext.jsx'
+import { MainRoute } from './routes/MainRoute.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
